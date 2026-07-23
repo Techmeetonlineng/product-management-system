00a82fccacd0a4ac20904986c0f3025a596a3fd2
Busari Nurudeen A. <nurudeenayoola002@gmail.com>
2026-07-23 23:52:48 +0100

    Cleaning up the image system

--- server/middleware/roleMiddleware.js
+++ server/middleware/roleMiddleware.js
@@ -3,35 +3,42 @@
 // ======================================
 
 function authorize(...allowedRoles) {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized.",
+      });
+    }
 
-    return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role_id)) {
+      return res.status(403).json({
+        success: false,
+        message: "You do not have permission to perform this action.",
+      });
+    }
 
-        if (!req.user) {
-
-            return res.status(401).json({
-
-                success: false,
-                message: "Unauthorized."
-
-            });
-
-        }
-
-        if (!allowedRoles.includes(req.user.role_id)) {
-
-            return res.status(403).json({
-
-                success: false,
-                message: "You do not have permission to perform this action."
-
-            });
-
-        }
-
-        next();
-
-    };
-
+    next();
+  };
 }
+
+module.exports = (...roles) => {
+  return (req, res, next) => {
+    console.log("========== ROLE CHECK ==========");
+    console.log("User:", req.user);
+    console.log("User Role:", req.user.role_id);
+    console.log("Allowed Roles:", roles);
+    console.log("================================");
+
+    if (!roles.includes(req.user.role_id)) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied.",
+      });
+    }
+
+    next();
+  };
+};
 
 module.exports = authorize;
