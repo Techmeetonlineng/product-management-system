b90613d9bfd7beb6d5ad7b36afed76ad75e84690
Busari Nurudeen A. <nurudeenayoola002@gmail.com>
2026-07-23 23:03:53 +0100

    Fix upload directory creation

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,6 +1,12 @@
 const { v2: cloudinary } = require("cloudinary");
 require("dotenv").config();
 
+console.log("========== CLOUDINARY ==========");
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("Secret Exists:", !!process.env.CLOUDINARY_API_SECRET);
+console.log("===============================");
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
